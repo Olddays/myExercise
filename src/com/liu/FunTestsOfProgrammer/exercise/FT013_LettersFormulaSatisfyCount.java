4103425612c3382490e1d9936699216effb22543
liuxudong03 <liuxudong03@baidu.com>
2018-03-29 21:02:27 +0800

    Change: add FT013

--- src/com/liu/FunTestsOfProgrammer/exercise/FT013_LettersFormulaSatisfyCount.java
+++ src/com/liu/FunTestsOfProgrammer/exercise/FT013_LettersFormulaSatisfyCount.java
@@ -1,9 +1,6 @@
 package com.liu.FunTestsOfProgrammer.exercise;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by liu on 218/02/28.
@@ -26,45 +23,94 @@ public class FT013_LettersFormulaSatisfyCount {
 
     private static class Equation {
         List<Character> character;
-        List<String> parameter;
+        List<String> parameters;
         List<Character> symbol;
         String equal;
 
-        Equation(List<Character> character, List<String> parameter,
+        Equation(List<Character> character, List<String> parameters,
                  List<Character> symbol, String equal) {
             this.character = character;
-            this.parameter = parameter;
+            this.parameters = parameters;
             this.symbol = symbol;
             this.equal = equal;
         }
 
         @Override
         public String toString() {
-            return "character: " + character + ", parameter: " + parameter
+            return "character: " + character + ", parameters: " + parameters
                     + ", symbol: " + symbol + ", equal: " + equal;
         }
     }
 
-    public static Map<Integer, String> getLettersFormulaSatisfyCountMy1(String equation) {
+    public static class Result {
+        String originString;
+        int result;
+        Map<String, Integer> wordMap;
+
+        Result(String originString, int result, Map<String, Integer> wordMap) {
+            this.originString = originString;
+            this.result = result;
+            this.wordMap = wordMap;
+        }
+
+        @Override
+        public String toString() {
+            return "originString: " + originString + ", result: " + result
+                    + ", wordMap: " + wordMap;
+        }
+    }
+
+    public static Result getLettersFormulaSatisfyCountMy1(String equation) {
         Equation equationInstance = getEquationInstance(equation);
         if (equationInstance == null) {
             return null;
         }
         System.out.println(equationInstance.toString());
-        Map<Integer, String> result = new HashMap<>();
-        return result;
+        boolean[] useableIndex = new boolean[10];
+        int[] numSet = new int['Z' - 'A'];
+        // 加个字母全排列的函数 返回一个list
+        getFullPermutation(equationInstance.character);
+        Map<String, Integer> wordMap = new HashMap<>();
+        int result = 0;
+        int parameterSize = equationInstance.parameters.size();
+        List<int[]> cache = new ArrayList<>();
+        int[] possible = new int[parameterSize + 1];
+        for (int i = 0; i < parameterSize + 1; i++) {
+            String target;
+            if (i == parameterSize) {
+                target = equationInstance.equal;
+            } else {
+                target = equationInstance.parameters.get(i);
+            }
+            char[] parameterSet = target.toCharArray();
+            int charSize = parameterSet.length;
+            int num = 1;
+            for (char parameter : parameterSet) {
+                num *= numSet[parameter - 'A'] * Math.pow(10, charSize);
+            }
+            possible[i] = num;
+        }
+        return new Result(equation, result, wordMap);
     }
 
-    public static Map<Integer, String> getLettersFormulaSatisfyCountMy2(String equation) {
-        Map<Integer, String> result = new HashMap<>();
-        return result;
+    public static Result getLettersFormulaSatisfyCountMy2(String equation) {
+        Map<String, Integer> wordMap = new HashMap<>();
+        int result = 0;
+        return new Result(equation, result, wordMap);
+    }
+
+    private static List<Map<Character, Integer>> getFullPermutation(List<Character> list) {
+        arrangementSelect(new String[]{
+                "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
+        }, list.size());
+        return null;
     }
 
     private static Equation getEquationInstance(String equation) {
         if (!equation.contains("=")) {
             return null;
         }
-        String[] parts = equation.toLowerCase()
+        String[] parts = equation.toUpperCase()
                 .replaceAll(" ", "").split("=");
         String equal = parts[1];
         char[] leftParts = parts[0].toCharArray();
@@ -73,7 +119,7 @@ public class FT013_LettersFormulaSatisfyCount {
         List<Character> symbol = new ArrayList<>();
         String cache = "";
         for (char c : leftParts) {
-            if (c <= 'z' && c >= 'a') {
+            if (c <= 'Z' && c >= 'A') {
                 if (!character.contains(c)) {
                     character.add(c);
                 }
@@ -84,6 +130,14 @@ public class FT013_LettersFormulaSatisfyCount {
                 symbol.add(c);
             }
         }
+        char[] rightParts = equal.toCharArray();
+        for (char c : rightParts) {
+            if (c <= 'Z' && c >= 'A') {
+                if (!character.contains(c)) {
+                    character.add(c);
+                }
+            }
+        }
         parameter.add(cache);
         if (parameter.size() != symbol.size() + 1
                 || character.size() > 10) {
@@ -91,5 +145,113 @@ public class FT013_LettersFormulaSatisfyCount {
             return null;
         }
         return new Equation(character, parameter, symbol, equal);
+    }
+
+    /**
+     * 排列选择（从列表中选择n个排列）
+     *
+     * @param dataList 待选列表
+     * @param n        选择个数
+     */
+    public static void arrangementSelect(String[] dataList, int n) {
+        System.out.println(String.format("A(%d, %d) = %d", dataList.length, n, arrangement(dataList.length, n)));
+        arrangementSelect(dataList, new String[n], 0);
+    }
+
+    /**
+     * 排列选择
+     *
+     * @param dataList    待选列表
+     * @param resultList  前面（resultIndex-1）个的排列结果
+     * @param resultIndex 选择索引，从0开始
+     */
+    private static void arrangementSelect(String[] dataList, String[] resultList, int resultIndex) {
+        int resultLen = resultList.length;
+        if (resultIndex >= resultLen) { // 全部选择完时，输出排列结果
+            System.out.println(Arrays.asList(resultList));
+            return;
+        }
+
+        // 递归选择下一个
+        for (int i = 0; i < dataList.length; i++) {
+            // 判断待选项是否存在于排列结果中
+            boolean exists = false;
+            for (int j = 0; j < resultIndex; j++) {
+                if (dataList[i].equals(resultList[j])) {
+                    exists = true;
+                    break;
+                }
+            }
+            if (!exists) { // 排列结果不存在该项，才可选择
+                resultList[resultIndex] = dataList[i];
+                arrangementSelect(dataList, resultList, resultIndex + 1);
+            }
+        }
+    }
+
+    /**
+     * 组合选择（从列表中选择n个组合）
+     *
+     * @param dataList 待选列表
+     * @param n        选择个数
+     */
+    public static void combinationSelect(String[] dataList, int n) {
+        System.out.println(String.format("C(%d, %d) = %d", dataList.length, n, combination(dataList.length, n)));
+        combinationSelect(dataList, 0, new String[n], 0);
+    }
+
+    /**
+     * 组合选择
+     *
+     * @param dataList    待选列表
+     * @param dataIndex   待选开始索引
+     * @param resultList  前面（resultIndex-1）个的组合结果
+     * @param resultIndex 选择索引，从0开始
+     */
+    private static void combinationSelect(String[] dataList, int dataIndex, String[] resultList, int resultIndex) {
+        int resultLen = resultList.length;
+        int resultCount = resultIndex + 1;
+        if (resultCount > resultLen) { // 全部选择完时，输出组合结果
+            System.out.println(Arrays.asList(resultList));
+            return;
+        }
+
+        // 递归选择下一个
+        for (int i = dataIndex; i < dataList.length + resultCount - resultLen; i++) {
+            resultList[resultIndex] = dataList[i];
+            combinationSelect(dataList, i + 1, resultList, resultIndex + 1);
+        }
+    }
+
+    /**
+     * 计算阶乘数，即n! = n * (n-1) * ... * 2 * 1
+     *
+     * @param n
+     * @return
+     */
+    public static long factorial(int n) {
+        return (n > 1) ? n * factorial(n - 1) : 1;
+    }
+
+    /**
+     * 计算排列数，即A(n, m) = n!/(n-m)!
+     *
+     * @param n
+     * @param m
+     * @return
+     */
+    public static long arrangement(int n, int m) {
+        return (n >= m) ? factorial(n) / factorial(n - m) : 0;
+    }
+
+    /**
+     * 计算组合数，即C(n, m) = n!/((n-m)! * m!)
+     *
+     * @param n
+     * @param m
+     * @return
+     */
+    public static long combination(int n, int m) {
+        return (n >= m) ? factorial(n) / factorial(n - m) / factorial(m) : 0;
     }
 }
--- src/com/liu/FunTestsOfProgrammer/test/FT013_LettersFormulaSatisfyCountTest.java
+++ src/com/liu/FunTestsOfProgrammer/test/FT013_LettersFormulaSatisfyCountTest.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import static com.liu.FunTestsOfProgrammer.exercise.FT013_LettersFormulaSatisfyCount.getLettersFormulaSatisfyCountMy1;
 import static com.liu.FunTestsOfProgrammer.exercise.FT013_LettersFormulaSatisfyCount.getLettersFormulaSatisfyCountMy2;
+import static com.liu.FunTestsOfProgrammer.exercise.FT013_LettersFormulaSatisfyCount.Result;
 
 /**
  * Created by liu on 2018/02/28.
@@ -15,7 +16,7 @@ public class FT013_LettersFormulaSatisfyCountTest {
                 "WE * LOVE = CODEIQ",
                 "READ + WRITE + TALK = SKILL"
         };
-        Map<Integer, String> result;
+        Result result;
         long startTime;
         long endTime;
 
@@ -23,28 +24,20 @@ public class FT013_LettersFormulaSatisfyCountTest {
             startTime = System.currentTimeMillis();
             result = testMy1(equation);
             endTime = System.currentTimeMillis();
-            System.out.println("LettersFormulaSatisfyCount My 1 result.size() " + result.size() + " during time " + (endTime - startTime));
-            for (Map.Entry entry : result.entrySet()) {
-                System.out.print(entry.getKey() + "-" + entry.getValue() + " ");
-            }
-            System.out.println();
+            System.out.println("LettersFormulaSatisfyCount My 1 result " + result + " during time " + (endTime - startTime));
 
             startTime = System.currentTimeMillis();
             result = testMy2(equation);
             endTime = System.currentTimeMillis();
-            System.out.println("LettersFormulaSatisfyCount My 2 result.size() " + result.size() + " during time " + (endTime - startTime));
-            for (Map.Entry entry : result.entrySet()) {
-                System.out.print(entry.getKey() + "-" + entry.getValue() + " ");
-            }
-            System.out.println();
+            System.out.println("LettersFormulaSatisfyCount My 2 result " + result + " during time " + (endTime - startTime));
         }
     }
 
-    private static Map<Integer, String> testMy1(String equation) {
+    private static Result testMy1(String equation) {
         return getLettersFormulaSatisfyCountMy1(equation);
     }
 
-    private static Map<Integer, String> testMy2(String equation) {
+    private static Result testMy2(String equation) {
         return getLettersFormulaSatisfyCountMy2(equation);
     }
 }
