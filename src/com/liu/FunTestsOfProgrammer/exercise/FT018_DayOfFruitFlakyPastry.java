b1134e33b6f4208754b0168a5a670660f1da2484
liuxudong <liuxudong03@baidu.com>
2018-04-19 21:12:04 +0800

    Change: init FT 18

--- src/com/liu/FunTestsOfProgrammer/exercise/FT018_DayOfFruitFlakyPastry.java
+++ src/com/liu/FunTestsOfProgrammer/exercise/FT018_DayOfFruitFlakyPastry.java
@@ -1,5 +1,8 @@
 package com.liu.FunTestsOfProgrammer.exercise;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by liu on 218/02/28.
  */
@@ -17,8 +20,113 @@ public class FT018_DayOfFruitFlakyPastry {
      * 求可以使切法满足条件的最小的 N（N ＞ 1）。
      */
 
+    // 方案可用，但是遍历时间太长
     public static int getDayOfFruitFlakyPastryMy1() {
-        int result = 0;
-        return result;
+        int result = 32;
+        while (true) {
+            int maxCount = result * (result + 1) / 2;
+            List<Integer> squareNumList = new ArrayList<>();
+            for (int i = 1; i * i <= maxCount; i++) {
+                squareNumList.add(i * i);
+            }
+            List<Integer> usableNumList = new ArrayList<>();
+            for (int i = 1; i <= result; i++) {
+                usableNumList.add(i);
+            }
+            if (checkSquareMy1(usableNumList, squareNumList)) {
+                return result;
+            }
+            result++;
+        }
+    }
+
+    private static boolean checkSquareMy1(List<Integer> usableNumList, List<Integer> squareNumList) {
+        List<int[]> list = new ArrayList<>();
+        int[] cache = new int[usableNumList.size()];
+        for (int i = 0; i < usableNumList.size(); i++) {
+            cache[i] = usableNumList.get(i);
+        }
+        getUsableNumSet(list, cache, 0, usableNumList.size());
+        for (int[] set : list) {
+            boolean allDataContain = true;
+            for (int i : set) {
+                if (!squareNumList.contains(i)) {
+                    allDataContain = false;
+                    break;
+                }
+            }
+            if (allDataContain) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void getUsableNumSet(List<int[]> list, int[] usableNumList, int start, int length) {
+        if (start == length - 1) {
+            int[] cache = new int[length];
+            for (int i = 0; i < length; i++) {
+                cache[i] = usableNumList[i] + usableNumList[(i + 1) % length];
+            }
+            if (!list.contains(cache)) {
+                list.add(cache);
+            }
+        } else {
+            for (int i = start; i < length; i++) {
+                int temp = usableNumList[start];
+                usableNumList[start] = usableNumList[i];
+                usableNumList[i] = temp;
+                getUsableNumSet(list, usableNumList, start + 1, length);
+                temp = usableNumList[start];
+                usableNumList[start] = usableNumList[i];
+                usableNumList[i] = temp;
+            }
+        }
+    }
+
+    public static int getDayOfFruitFlakyPastryMy2() {
+        int result = 32;
+        while (true) {
+            int maxCount = result * 2 - 1; // 相邻的两块最大合
+            List<Integer> squareNumList = new ArrayList<>();
+            for (int i = 1; i * i <= maxCount; i++) {
+                squareNumList.add(i * i);
+            }
+            List<Integer> usableNumList = new ArrayList<>();
+            for (int i = 1; i <= result; i++) {
+                usableNumList.add(i);
+            }
+            List<Integer> log = new ArrayList<>();
+            log.add(1);
+            if (checkSquareMy2(result, 1, log, squareNumList, usableNumList)) {
+                return result;
+            }
+            result++;
+        }
+    }
+
+    // 先计算好都有哪些可用的平方数，然后进行判断。
+    // 切分后是个循环队列，所以可以认为第一个恒定为1，因为是可以循环到1开始的。
+    // 在处理过程中保证每次下一个都满足平方数的条件即可。
+    private static boolean checkSquareMy2(int n, int pre, List<Integer> log, List<Integer> squareNumList, List<Integer> usableNumList) {
+        if (n == log.size()) {
+            if (squareNumList.contains(1 + pre)) {
+                return true;
+            }
+        } else {
+            for (int i : usableNumList) {
+                if (!log.contains(i)) {
+                    if (squareNumList.contains(pre + i)) {
+                        log.add(i);
+                        boolean checked = checkSquareMy2(n, i, log, squareNumList, usableNumList);
+                        if (checked) {
+                            return true;
+                        }
+                        log.remove(new Integer(i));
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
--- src/com/liu/FunTestsOfProgrammer/test/FT018_DayOfFruitFlakyPastryTest.java
+++ src/com/liu/FunTestsOfProgrammer/test/FT018_DayOfFruitFlakyPastryTest.java
@@ -1,6 +1,7 @@
 package com.liu.FunTestsOfProgrammer.test;
 
 import static com.liu.FunTestsOfProgrammer.exercise.FT018_DayOfFruitFlakyPastry.getDayOfFruitFlakyPastryMy1;
+import static com.liu.FunTestsOfProgrammer.exercise.FT018_DayOfFruitFlakyPastry.getDayOfFruitFlakyPastryMy2;
 
 /**
  * Created by liu on 2018/02/28.
@@ -13,12 +14,22 @@ public class FT018_DayOfFruitFlakyPastryTest {
         long endTime;
 
         startTime = System.currentTimeMillis();
-        result = testMy1();
+//        result = testMy1();
+        result = 0;
         endTime = System.currentTimeMillis();
         System.out.println("DayOfFruitFlakyPastry My 1 result " + result + " during time " + (endTime - startTime));
+
+        startTime = System.currentTimeMillis();
+        result = testMy2();
+        endTime = System.currentTimeMillis();
+        System.out.println("DayOfFruitFlakyPastry My 2 result " + result + " during time " + (endTime - startTime));
     }
 
     private static int testMy1() {
         return getDayOfFruitFlakyPastryMy1();
+    }
+
+    private static int testMy2() {
+        return getDayOfFruitFlakyPastryMy2();
     }
 }
