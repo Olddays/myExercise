cf1768e84a349b4f064c697fa1e8fc6dbfc15101
Xudong Liu <lls6145219@gmail.com>
2018-05-30 23:46:35 +0800

    Change: init FT024

--- src/com/liu/FunTestsOfProgrammer/exercise/FT024_PerfectStrikeOut.java
+++ src/com/liu/FunTestsOfProgrammer/exercise/FT024_PerfectStrikeOut.java
@@ -1,6 +1,8 @@
 package com.liu.FunTestsOfProgrammer.exercise;
 
 
+import javafx.util.Pair;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,12 +29,88 @@ public class FT024_PerfectStrikeOut {
      * 能一次击落2个靶子，已经击落的靶子下次就不能用了，譬如1号被击落后，与1号靶子相邻的地方就不能再一次击落2个了。
      */
 
+    // 问题的关键在于，已经击落的靶子就不能再次击中。
+    // 1 号被击落后，“1 和 2”“ 1 和 4”就不能再适用“一次性击落”这种情况了。
+    // 反过来说，如果一次性击落“1 和 2”，则 1 号和2 号靶子就都不能再使用了。
+    // 为实现这样的思路，这里用数组来表示击打方法。
+    // 另外，无论是按照1 号 ~3 号的顺序击球，还是按照3 号 ~1 号的顺序击球，这之后的模式都是一样的。
+    // 因此可以把已遍历的靶子内存化，从而优化处理速度。
     public static int getPerfectStrikeOutMy1(int length, int width) {
-        return 0;
+        int num = 1;
+        int[][] cache = new int[length][width];
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                cache[i][j] = num++;
+            }
+        }
+        List<int[]> doubleShot = getDoubleShotList(cache, length, width);
+        doubleShot.set(0, new int[]{cache[length / 2][width / 2]});
+        for (int i = 1; i <= length * width; i++) {
+            if (i == (length * width + 1) / 2) {
+                continue;
+            }
+            doubleShot.add(new int[]{i});
+        }
+        List<String> memory = new ArrayList<>();
+        List<List<String>> result = new ArrayList<>();
+        countPossibleCombine(result, doubleShot, memory, 0, length * width);
+        return result.size();
     }
 
-    public static int getPerfectStrikeOutMy2(int length, int width) {
-        return 0;
+    private static void countPossibleCombine(List<List<String>> result, List<int[]> doubleShot, List<String> memory,
+                                             int start, int size) {
+        if (memory.size() == size) {
+            result.add(new ArrayList<>(memory));
+        } else {
+            for (int i = start; i < doubleShot.size(); i++) {
+                boolean hasNum = false;
+                for (int num : doubleShot.get(i)) {
+                    if (memory.contains(String.valueOf(num))) {
+                        hasNum = true;
+                        break;
+                    }
+                }
+                if (!hasNum) {
+                    for (int num : doubleShot.get(i)) {
+                        memory.add(String.valueOf(num));
+                    }
+                    countPossibleCombine(result, doubleShot, memory, start + 1, size);
+                    for (int num : doubleShot.get(i)) {
+                        memory.remove(String.valueOf(num));
+                    }
+                } else {
+                    countPossibleCombine(result, doubleShot, memory, start + 1, size);
+                }
+            }
+        }
+    }
+
+    private static List<int[]> getDoubleShotList(int[][] cache, int length, int width) {
+        List<int[]> result = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                if (i < length - 1 && j < width - 1) {
+                    if (i == length / 2 && j == width / 2) {
+                        continue;
+                    }
+                    if (i + 1 != length / 2 || j != width / 2) {
+                        int[] pair = {cache[i][j], cache[i + 1][j]};
+                        result.add(pair);
+                    }
+                    if (i != length / 2 || j + 1 != width / 2) {
+                        int[] pair = {cache[i][j], cache[i][j + 1]};
+                        result.add(pair);
+                    }
+                } else if (i == length - 1 && j < width - 1) {
+                    int[] pair = {cache[i][j], cache[i][j + 1]};
+                    result.add(pair);
+                } else if (i < length - 1 && j == width - 1) {
+                    int[] pair = {cache[i][j], cache[i + 1][j]};
+                    result.add(pair);
+                }
+            }
+        }
+        return result;
     }
 
 }
--- src/com/liu/FunTestsOfProgrammer/test/FT024_PerfectStrikeOutTest.java
+++ src/com/liu/FunTestsOfProgrammer/test/FT024_PerfectStrikeOutTest.java
@@ -1,7 +1,6 @@
 package com.liu.FunTestsOfProgrammer.test;
 
 import static com.liu.FunTestsOfProgrammer.exercise.FT024_PerfectStrikeOut.getPerfectStrikeOutMy1;
-import static com.liu.FunTestsOfProgrammer.exercise.FT024_PerfectStrikeOut.getPerfectStrikeOutMy2;
 
 /**
  * Created by liu on 2018/02/28.
@@ -10,13 +9,9 @@ public class FT024_PerfectStrikeOutTest {
 
     public static void main(String[] args) {
         int[] lengthSet = {
-                2,
-                2,
                 3
         };
         int[] weightSet = {
-                1,
-                2,
                 3
         };
         int result;
@@ -31,19 +26,10 @@ public class FT024_PerfectStrikeOutTest {
             result = testMy1(length, weight);
             endTime = System.currentTimeMillis();
             System.out.println("PerfectStrikeOut My 1 result " + result + " during time " + (endTime - startTime));
-
-            startTime = System.currentTimeMillis();
-            result = testMy2(length, weight);
-            endTime = System.currentTimeMillis();
-            System.out.println("PerfectStrikeOut My 2 result " + result + " during time " + (endTime - startTime));
         }
     }
 
     private static int testMy1(int length, int weight) {
         return getPerfectStrikeOutMy1(length, weight);
-    }
-
-    private static int testMy2(int length, int weight) {
-        return getPerfectStrikeOutMy2(length, weight);
     }
 }
